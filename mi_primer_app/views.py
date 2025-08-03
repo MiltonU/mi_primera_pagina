4635a8681f4feaf195c78c7a53034bd08d07c6fe
Milton Ugartemendia <milton.ugartemendia95@gmail.com>
2025-08-03 18:28:54 -0300

    login

--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -6,5 +6,5 @@ def hola_mundo(request):
     return HttpResponse('¡Hola, mundo!')
 
 def home(request):
-    return HttpResponse('¡Bienvenido a mi Home page!')
+    return render(request,'mi_primer_app/home.html')
 
