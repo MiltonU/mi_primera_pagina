147bc2fc3a41ee2fb093f3029b95beeb9bf7ec07
Milton Ugartemendia <milton.ugartemendia95@gmail.com>
2025-08-15 12:51:47 -0300

    Actualización: se agregó formulario de vinos y vista de compra

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    # 🏠 Inicio (verificación incluida en home)
     path('', views.home, name='home'),
-    path('acceso-denegado/', views.rechazo, name='rechazo'),
-
-    # 🔐 Autenticación
-    path('login/', views.mostrar_login, name='mostrar_login'),
-    path('registro/', views.registro_usuario, name='registro'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
-
-    # 📥 Ingreso de datos
-    path('formulario/', views.crear_objetos, name='formulario'),
-
-    # 🔍 Búsqueda
-    path('buscar/', views.buscar_vinos, name='buscar_vinos'),
+    path('login/', views.login_view, name='login'),
+    path('registro/', views.registro_view, name='registro'),
 ]
