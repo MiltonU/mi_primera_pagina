d57573d8d29584222fd22b90860fda301bf8006f
Milton Ugartemendia <milton.ugartemendia95@gmail.com>
2025-08-26 13:30:44 -0300

    updates

--- Mi_Primera_Pagina/settings.py
+++ Mi_Primera_Pagina/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'ckeditor',
-    'accounts',
     'messenger',
-    
+    'accounts.apps.AccountsConfig',    
 ]
 
 
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Profile
 
+User = get_user_model()
+
 @receiver(post_save, sender=User)
-def crear_perfil(sender, instance, created, **kwargs):
+def crear_perfil_sensitivo(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
